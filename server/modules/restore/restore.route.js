9cc538d533435377465a30eadbeb4f5243817465
ben <ben@surgison.net>
2019-09-03 08:59:35 +0100

    Fix restore

--- server/modules/restore/restore.route.js
+++ server/modules/restore/restore.route.js
@@ -1,7 +1,10 @@
+const { Cache } = require('ivory-shared')
 const syncRegistration = require('../../lib/sync-registration')
 
 class RestoreHandlers extends require('../common/handlers') {
   async handleGet (request, h, errors) {
+    // Clear the cookies and create a new registration
+    await Cache.clear(request)
     const { id } = request.params
     await syncRegistration.restore(request, id)
     return h.redirect('/check-your-answers')
