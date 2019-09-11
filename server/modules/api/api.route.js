7c13d162347154a46666a370e97db14d7fc8d192
ben <ben@surgison.net>
2019-09-11 15:20:54 +0100

    Add api proxy for service testing

---
+++ server/modules/api/api.route.js
@@ -0,0 +1,24 @@
+const { Persistence } = require('ivory-shared')
+const querystring = require('querystring')
+const { serviceApi, isProd } = require('../../config')
+
+const routes = []
+
+if (!isProd) {
+  routes.push({
+    method: 'GET',
+    path: '/api/{model}/{id?}',
+    handler: async (request) => {
+      const query = querystring.stringify(request.query)
+      const path = `${serviceApi}${request.path}${query ? `?${query}` : ''}`.replace('/api', '')
+      const persistence = new Persistence({ path })
+      return persistence.restore()
+    },
+    options: {
+      tags: ['api'],
+      security: true
+    }
+  })
+}
+
+module.exports = routes
