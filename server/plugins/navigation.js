5ef27c81967e50868ef90f70db112122ff40a45b
ben <ben@surgison.net>
2019-09-24 14:03:21 +0100

    Do not prevent back when submitted (for now)

--- server/plugins/navigation.js
+++ server/plugins/navigation.js
@@ -8,7 +8,7 @@ module.exports = {
       server.ext('onPostAuth', async (request, h) => {
         const { settings } = request.route
         const { tags = [] } = settings
-        const { status, registrationNumber } = await Registration.get(request) || {}
+        const registration = await Registration.get(request) || {}
 
         // Always allow the following to continue
         if (tags.includes('always')) {
@@ -17,13 +17,15 @@ module.exports = {
 
         // Check submitted tag
         if (tags.includes('submitted')) {
-          if (status !== 'submitted') {
+          if (registration.status !== 'submitted') {
             return Boom.notFound()
           }
-        } else {
-          if (status === 'submitted') {
-            return Boom.preconditionFailed('Registration already submitted', { registrationNumber })
-          }
+        // ToDo: Need to support already submitted when designed
+        // } else {
+        //   const { status, registrationNumber } = registration
+        //   if (status === 'submitted') {
+        //     return Boom.preconditionFailed('Registration already submitted', { registrationNumber })
+        //   }
         }
         return h.continue
       })
