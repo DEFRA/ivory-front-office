5cb874b08966b0a04399a509d4fd72229ada9565
ben <ben@surgison.net>
2019-09-27 12:05:00 +0100

    Check your answers

--- server/modules/check-your-answers/check-your-answers.route.test.js
+++ server/modules/check-your-answers/check-your-answers.route.test.js
@@ -21,7 +21,12 @@ lab.experiment(TestHelper.getFile(__filename), () => {
         },
         itemType: {
           choices: [
-            { shortName: 'portrait-miniature-pre-1918', label: 'portrait-miniature-pre-1918' }
+            {
+              shortName: 'portrait-miniature-pre-1918',
+              label: 'portrait-miniature-pre-1918',
+              ageExemptionDeclaration: 'the item was made before 1918',
+              volumeExemptionDeclaration: 'the portrait miniature is less than 320cm²'
+            }
           ]
         }
       })
@@ -29,28 +34,46 @@ lab.experiment(TestHelper.getFile(__filename), () => {
 
     lab.test('page answers are displayed correctly', async ({ context }) => {
       const agentIsOwner = true
-      const agentActingAs = 'trustee'
       const dealingIntent = 'hire'
       const itemType = 'portrait-miniature-pre-1918'
       const fullName = 'James Bond'
+      const email = 'james007@bond.co.uk'
       const addressLine = 'THRIVE RENEWABLES PLC, DEANERY ROAD, BRISTOL, BS1 5AH'
       const description = 'A violin bow with an ivory tip.'
+      const ageExemptionDeclaration = true
+      const ageExemptionDescription = 'an explanation for age exemption'
+      const volumeExemptionDeclaration = true
+      const volumeExemptionDescription = 'an explanation for volume exemption'
+      const ageExemptionDeclarationLabel = 'I declare the item was made before 1918'
+      const volumeExemptionDeclarationLabel = 'I declare the portrait miniature is less than 320cm²'
 
       Object.assign(routesHelper.cache, {
-        Registration: { agentIsOwner, agentActingAs, dealingIntent },
-        Owner: { fullName },
+        Registration: { dealingIntent, agentIsOwner },
+        Owner: { fullName, email },
         OwnerAddress: { addressLine },
-        Item: { description, itemType }
+        Item: {
+          description,
+          itemType,
+          ageExemptionDeclaration,
+          ageExemptionDescription,
+          volumeExemptionDeclaration,
+          volumeExemptionDescription
+        }
       })
 
       const response = await routesHelper.server.inject(context.request)
       const $ = routesHelper.getDomParser(response.payload)
 
-      Code.expect($('#owner-full-name').text()).to.include(fullName)
-      Code.expect($('#owner-address-line').text()).to.include(addressLine)
-      Code.expect($('#item-description').text()).to.include(description)
-      Code.expect($('#dealing-intent').text()).to.include(dealingIntent)
-      Code.expect($('#item-type').text()).to.include(itemType)
+      Code.expect($('.ivory-item-type').text()).to.include(itemType)
+      Code.expect($('.ivory-description').text()).to.include(description)
+      Code.expect($('.ivory-age-of-ivory').text()).to.include(ageExemptionDescription)
+      Code.expect($('.ivory-age-of-ivory').text()).to.include(ageExemptionDeclarationLabel)
+      Code.expect($('.ivory-volume-of-ivory').text()).to.include(volumeExemptionDescription)
+      Code.expect($('.ivory-volume-of-ivory').text()).to.include(volumeExemptionDeclarationLabel)
+      Code.expect($('.ivory-your-name').text()).to.include(fullName)
+      Code.expect($('.ivory-your-address').text()).to.include(addressLine.split(',').join(''))
+      Code.expect($('.ivory-your-email').text()).to.include(email)
+      Code.expect($('.ivory-intention').text()).to.include(dealingIntent)
     })
   })
 })
