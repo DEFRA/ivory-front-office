9aa250f08f1da4f98e083b993914e9cadddbf8bf
ben <ben@surgison.net>
2019-10-31 09:04:58 +0000

    Add started timestamp to version route

--- server/modules/version/version.route.js
+++ server/modules/version/version.route.js
@@ -8,6 +8,7 @@ const git = require('git-last-commit')
 class VersionHandlers extends require('ivory-common-modules').handlers {
   // Overrides parent class handleGet
   async handleGet (request, h, errors) {
+    const { info: instance } = request.server
     const commit = await new Promise((resolve, reject) => {
       git.getLastCommit((err, commit) => {
         if (err) {
@@ -17,8 +18,12 @@ class VersionHandlers extends require('ivory-common-modules').handlers {
         }
       })
     })
-    Object.assign(commit, { name, version, commit: homepage.replace('#readme', `/commit/${commit.hash}`) })
+    Object.assign(commit, { name, version, commit: homepage.replace('#readme', `/commit/${commit.hash}`), instance })
     const services = [commit, await persistence.restore()]
+    services.forEach((service) => {
+      const { instance } = service
+      instance.startedTimestamp = moment(instance.started).format('DD/MM/YYYY HH:mm:ss')
+    })
     this.viewData = { services, renderTimestamp: moment().format('DD/MM/YYYY HH:mm:ss') }
     return super.handleGet(request, h, errors)
   }
