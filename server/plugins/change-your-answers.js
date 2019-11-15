f0b45ea7eed66c684c0b21e9aa9707c96adde1e8
ben <ben@surgison.net>
2019-11-15 11:07:01 +0000

    Clean-up change your answers plugin

--- server/plugins/change-your-answers.js
+++ server/plugins/change-your-answers.js
@@ -1,71 +1,24 @@
 const { Cache } = require('ivory-shared')
 const { Registration } = require('ivory-data-mapping').cache
 const { flow } = require('../flow')
-
-const options = {
-  validData: async (request) => {
-    const { validForPayment } = await Registration.get(request) || {}
-    return validForPayment
-  },
-  ignoreRoute: async ({ route }) => {
-    const { tags = [] } = route.settings
-    return (tags.includes('always'))
-  },
-  checkYourAnswersPath: flow['check-your-answers'].path,
-  setChanging: async (request, flag) => Cache.set(request, 'Changing', flag),
-  isChanging: async (request) => Cache.get(request, 'Changing')
-}
-
-function changeYourAnswers (server, options) {
-  server.ext('onPostAuth', async (request, h) => {
-    const { ignoreRoute, checkYourAnswersPath, setChanging } = options
-
-    if (await ignoreRoute(request)) {
-      return h.continue
-    }
-
-    const { method, route } = request
-
-    if (method !== 'get') {
-      return h.continue
-    }
-
-    if (route.path !== checkYourAnswersPath) {
-      // Set status to checking if referred from check your answers
-      const { referrer } = request.info
-      const referrerPath = referrer ? (new URL(referrer)).pathname : ''
-      if (referrerPath === checkYourAnswersPath) {
-        await setChanging(request, true)
-      }
-    }
-
-    return h.continue
-  })
-
-  server.ext('onPostHandler', async (request, h) => {
-    const { ignoreRoute, checkYourAnswersPath, setChanging, isChanging, validData } = options
-
-    if (await ignoreRoute(request)) {
-      return h.continue
-    }
-
-    const { method } = request
-
-    if (method === 'post' && await isChanging(request)) {
-      if (await validData(request)) {
-        // Make sure Changing is switched off
-        await setChanging(request, false)
-        return h.redirect(checkYourAnswersPath).takeover()
-      }
-    }
-
-    return h.continue
-  })
-}
+const { changeYourAnswers } = require('ivory-common-modules').plugins
 
 module.exports = {
+  options: {
+    validData: async (request) => {
+      const { validForPayment } = await Registration.get(request) || {}
+      return validForPayment
+    },
+    ignoreRoute: async ({ route }) => {
+      const { tags = [] } = route.settings
+      return (tags.includes('always'))
+    },
+    checkYourAnswersPath: flow['check-your-answers'].path,
+    setChanging: async (request, flag) => Cache.set(request, 'Changing', flag),
+    isChanging: async (request) => Cache.get(request, 'Changing')
+  },
   plugin: {
     name: 'change-your-answers',
-    register: (server) => changeYourAnswers(server, options)
+    register: changeYourAnswers
   }
 }
