a47e4cdb72ff0f180fe7f6bee3bd52766df80d83
ben <ben@surgison.net>
2019-12-16 13:03:12 +0000

    Proxy swagger through the front end

--- server/plugins/service-api-proxy.js
+++ server/plugins/service-api-proxy.js
@@ -1,12 +1,39 @@
 const { serviceApi } = require('../config')
 
 module.exports = {
-  plugin: require('hapi-proxy-get'),
-  options: {
-    uri: serviceApi,
-    path: '/api',
-    options: {
-      tags: ['api', 'always']
+  plugin: {
+    name: 'defra-ivory-service-api-proxy',
+    register: (server) => {
+      server.register([
+        {
+          plugin: require('hapi-proxy-get'),
+          options: {
+            uri: serviceApi,
+            path: '/api',
+            options: {
+              tags: ['api', 'always']
+            }
+          }
+        }, {
+          plugin: require('hapi-proxy-get'),
+          options: {
+            uri: `${serviceApi}/swaggerui`,
+            path: '/swaggerui',
+            options: {
+              tags: ['always']
+            }
+          }
+        }, {
+          plugin: require('hapi-proxy-get'),
+          options: {
+            uri: `${serviceApi}`,
+            path: '',
+            options: {
+              tags: ['always']
+            }
+          }
+        }
+      ])
     }
   }
 }
