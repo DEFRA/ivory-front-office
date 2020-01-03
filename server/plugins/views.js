b6eede8c0f5f02f648b1726a75bcccc07f84faa2
ben <ben@surgison.net>
2020-01-03 13:31:41 +0000

    Adding hapi-govuk-plugin part 1

--- server/plugins/views.js
+++ server/plugins/views.js
@@ -1,69 +1,20 @@
-const nunjucks = require('nunjucks')
-const config = require('../config')
+const { analyticsAccount, serviceName } = require('../config')
 const pkg = require('../../package.json')
-const analyticsAccount = config.analyticsAccount
-const glob = require('glob')
 
-const moduleTemplates = glob.sync('./*/*/!(node_modules)/**/*.njk')
+const viewOptions = require('hapi-govuk-frontend/lib/view-options')
 
-const macroFolders = [...new Set(moduleTemplates
-  .filter((file) => file.endsWith('/macro.njk'))
-  .map((file) => file
-    .split('/')
-    .slice(0, -2)
-    .join('/')
-  ))]
-
-const folders = moduleTemplates
-  .reduce((folders = [], file) => {
-    if (!macroFolders.filter((folder) => file.startsWith(folder)).length) {
-      const folder = file
-        .split('/')
-        .slice(0, -1)
-        .join('/')
-      if (!folders.includes(folder)) {
-        folders.push(folder)
-      }
-    }
-    return folders
-  }, macroFolders)
-  .map((folder) => folder.substring(2))
-  .sort()
+const context = {
+  appVersion: pkg.version,
+  assetPath: '/assets',
+  serviceName,
+  pageTitle: serviceName + ' - GOV.UK',
+  analyticsAccount: analyticsAccount
+}
 
 module.exports = {
   plugin: require('@hapi/vision'),
-  options: {
-    engines: {
-      njk: {
-        compile: (src, options) => {
-          const template = nunjucks.compile(src, options.environment)
-
-          return (context) => {
-            return template.render(context)
-          }
-        },
-
-        prepare: (options, next) => {
-          options.compileOptions.environment = nunjucks.configure(
-            folders,
-            {
-              autoescape: true,
-              watch: false
-            })
-
-          return next()
-        }
-      }
-    },
-    path: '../modules',
-    relativeTo: __dirname,
-    isCached: !config.isDev,
-    context: {
-      appVersion: pkg.version,
-      assetPath: '/assets',
-      serviceName: config.serviceName,
-      pageTitle: config.serviceName + ' - GOV.UK',
-      analyticsAccount: analyticsAccount
-    }
-  }
+  options: viewOptions({
+    viewPath: 'server/modules',
+    context
+  })
 }
